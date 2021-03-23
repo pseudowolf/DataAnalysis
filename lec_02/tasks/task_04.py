9a5239940bd339f10cf661aac60de9c5cfc68d92
pseudowolf <ed.suzi@gmal.com>
2021-03-23 13:44:25 +0200

    task 4.

--- lec_02/tasks/task_04.py
+++ lec_02/tasks/task_04.py
@@ -1,8 +1,13 @@
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 
 
 def get_combinations(s, k) -> tuple:
-    return tuple(combinations(s, k))
+    return list(combinations(s, k))
+
+def get_combinations_with_replacement(s, k):
+    return list(combinations_with_replacement(s, k))
 
 
-print(get_combinations('ABCDE', 4))
+print(get_combinations('ABCDE', 2))
+print(get_combinations_with_replacement('abcd', 2))
+
